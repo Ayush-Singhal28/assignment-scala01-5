bcd22f8bfec111cb693b5d94e710fb368f0eab51
Ayush-Singhal28 <ayush.singhal@knoldus.in>
2018-01-15 16:13:31 +0530

    Update collatz.scala

--- src/main/scala/edu/knoldus/collatz.scala
+++ src/main/scala/edu/knoldus/collatz.scala
@@ -2,14 +2,14 @@ package edu.knoldus
 
 import org.apache.log4j.Logger
 
-object Activity extends App {
-  def main(args: String): Unit = {
+object Activity {
+  def main(args: Array[String]): Unit = {
     val log = Logger.getLogger(this.getClass)
     val list = List()
     val inputValue1 = 27
     val inputValue2 = 38
     val inputValue3 = 17
-    val process = new Application
+    val process = new Activity
     val list1: List[Int] = process.collatzEquation(inputValue1, list)
     log.info("New List1\n" + list1)
     val list2: List[Int] = process.collatzEquation(inputValue1, list1)
@@ -19,7 +19,7 @@ object Activity extends App {
   }
 }
 
-class Application {
+class Activity {
   def collatzEquation(inputValue: Int, list: List[Int]): List[Int] = {
     list match {
       case Nil => list.::(inputValue)
@@ -28,20 +28,16 @@ class Application {
   }
 
   def computationPerformed(list: List[Int]): List[Int] = {
-
     val element = list(0)
-    if (element != 1) {
-      if (element % 2 == 0) {
-        val newElement = element / 2
-        list.::(newElement)
-      }
-      else {
-        val newElement = 3 * element + 1
-        list.::(newElement)
-      }
+    element match {
+      case 1 => list
+      case element if (element % 2 == 0) => computationPerformed(element / 2 :: list)
+      case _ => computationPerformed((3 * element) + 1 :: list)
+
     }
-    List()
   }
+    
+      
 }
 
 
